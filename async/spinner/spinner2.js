2af6901c3bc07dc87063255b907b31f77e99bef5
Abdulahi hirsi <110142610+ahirsi1215@users.noreply.github.com>
2022-09-28 19:25:39 -0400

    Delete async directory

--- async/spinner/spinner2.js
+++
@@ -1,9 +0,0 @@
-const spins = (['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']);
-let counter = 100;
-const spinner = function () {
-  for (let sp of spins) {
-    setTimeout(() => {
-      process.stdout.write('\r' + sp);
-    }, counter += 200);
-  }
-}
