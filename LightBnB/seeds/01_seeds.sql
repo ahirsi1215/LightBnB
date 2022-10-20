e915a885898484ab0d3abfff02375713e5564ca4
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-20 00:31:50 +0000

    initial push

---
+++ LightBnB/seeds/01_seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO users VALUES (1,'Eva Stanley ','sebastianguerra@ymail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2,'Louisa Meyer','jacksonrose@hotmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3,'Dominic Parks','victoriablackwell@outlook.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties VALUES (1,1,'Speed lamp','description','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',$930.61 ,6,4,8,'Canada','536 Namsub Highway ',' Sotboske ','Quebec ','28142',TRUE) 
+(2,1,'Blank corner ','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350 ','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',85214,6,8,7,'Canada','651 Nami Road  ',' Bohbatev ','Alberta ','83600',TRUE),
+(3,2,'Habit mix','description','imgager','imgage',205418,2,3,3,'Canada','Holand st.',' Genwezuj ','Newfoundland And Labrador','45583',TRUE);
+
+INSERT INTO reservations VALUES (1,1,1,'2018-09-11','2018-09-26'),
+(2,1,1,'2019-01-04 ','2019-02-01'),
+(3,2,3,'2023-10-01','2023-10-14');
+
+INSERT INTO property_reviews VALUES (1,4,5,1,2,'Nice'),
+(1,3,3,5,4,'Cool'),
+(2,1,4,2,2,'Great');
